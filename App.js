1e1ab6e99880f8b3a6f13c38e0ff5b25cae30226
zukyCoder <zukydeveloper@gmail.com>
2024-10-29 17:56:22 +0100

    create dynamic view and add specific styles for each device

--- App.js
+++ App.js
@@ -32,7 +32,9 @@ const [courseGoals, setCourseGoals] = useState([]);
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+        {courseGoals.map((goal) => <View  key={goal} style={styles.goalItem}>
+          <Text style={styles.goalText}>{goal}</Text>
+        </View>)}
       </View>    
     </View>
   );
@@ -63,5 +65,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white'
   }
 });
