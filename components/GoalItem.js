89c7da3638c1024a3c623086c6a58b92ba44fdf4
zukyCoder <zukydeveloper@gmail.com>
2024-10-31 10:46:07 +0100

    add style for ios

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,18 @@
 import { StyleSheet, 
          Text, 
-         View 
+         View,
+         Pressable
         } from 'react-native';
 function GoalItem(props) {
     return (
         <View style={stylesItems.goalItem}>
-            <Text style={stylesItems.goalText}>{props.text}</Text>
+            <Pressable 
+            android_ripple={{color: '#4b0082'}} 
+            //onPress={props.onDeleteItem.bind(this, props.id)}
+            style={({pressed}) => pressed && stylesItems.pressedItem}
+            >
+                <Text style={stylesItems.goalText}>{props.text}</Text>
+            </Pressable>
         </View>
     );
 };
@@ -15,11 +22,14 @@ export default GoalItem;
 const stylesItems = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
   goalText: {
     color: 'white',
+    padding: 8,
   },
+  pressedItem: {
+    opacity: 0.5
+  }
 });
